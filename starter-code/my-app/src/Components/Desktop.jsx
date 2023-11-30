d0f462e81366612a92eb644ea21620c4db94b766
shakomelikidze <shako456@gmail.com>
2023-11-30 14:04:59 +0400

    .

---
+++ starter-code/my-app/src/Components/Desktop.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Desktop() {
+  return (
+    <>
+      <div className="planets-list">
+        asdasdasdasd
+      </div>
+    </>
+  )
+}
+
+export default Desktop
---
+++ starter-code/my-app/src/Components/Mobile.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+// import burger from '../assets/icon-hamburger.svg';
+
+function Mobile() {
+  return (
+    <>
+      <nav>
+          {/* <p className='logo'>The planets</p>
+          <button >
+            <img className='burger' src={burger} alt="burger" />
+          </button> */}
+          <div className="planets-list">
+            asdasdadasdasd
+          </div>
+      </nav>
+    </>
+  )
+}
+
+export default Mobile
