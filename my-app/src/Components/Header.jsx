e31ad47008de87769b3571254f9d006508d2124a
shakomelikidze <shako456@gmail.com>
2023-12-15 18:22:11 +0400

    .
    desktopis patara shesworeba

--- my-app/src/Components/Header.jsx
+++ my-app/src/Components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Burger from '../../assets/icon-hamburger.svg'
 
 function Header() {
   return (
@@ -7,7 +8,7 @@ function Header() {
       <nav>
         <h1 className='logo'>The planets</h1>
         <div className="planets-list">
-          <Link to='mercury'>Mercuty</Link>
+          <Link to='mercury'>Mercury</Link>
           <Link to='venus'>Venus</Link>
           <Link to='earth'>Earth</Link>
           <Link to='mars'>Mars</Link>
@@ -16,6 +17,9 @@ function Header() {
           <Link to='uranus'>Uranus</Link>
           <Link to='neptune'>Neptune</Link>
         </div>
+        <button className='burger'>
+          <img src={Burger} alt="" />
+        </button>
       </nav>
       <hr style={{borderColor: '#38384F'}} />
       
--- my-app/src/pages/Planet.jsx
+++ my-app/src/pages/Planet.jsx
@@ -2,16 +2,7 @@ import React from 'react'
 import {useParams} from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import data from '../../../data.json'
-import Mercury from '../../assets/planet-mercury.svg'
-import Venus from '../../assets/planet-venus.svg'
-import Earth from '../../assets/planet-earth.svg'
-import Mars from '../../assets/planet-mars.svg'
-import Jupiter from '../../assets/planet-jupiter.svg'
-import Saturn from '../../assets/planet-saturn.svg'
-import Uranus from '../../assets/planet-uranus.svg'
-import Neptune from '../../assets/planet-neptune.svg'
 import Source from '../../assets/icon-source.svg'
-
 
 function Planet() {
   const params = useParams();
@@ -34,28 +25,32 @@ function Planet() {
       <section>
       {planetImage && <img className='planet-img' src={planetImage} alt={planetData.name} />}
         <div className="info-box">
-          <p className="planet-name">{planetData.name}</p>
-          <p className="about">{planetData.overview.content}</p>
-          <p className='source'>
-            Source :  
-            <p className='wikipedia-style'>
-              Wikipedia
+          <div>
+            <p className="planet-name">{planetData.name}</p>
+            <p className="about">{planetData.overview.content}</p>
+            <p className='source'>
+              Source :  
+              <p className='wikipedia-style'>
+                Wikipedia
+              </p>
+              <img style={{width: '12px', height: '12px'}} src={Source} alt="source" />
             </p>
-            <img style={{width: '12px', height: '12px'}} src={Source} alt="source" />
-          </p>
-          {/* Here is overview, internal and surface buttons */}
-          <button className='info-type-btn'>
-            01 
-            <p className='ss'>Overview</p>
-          </button>
-          <button className='info-type-btn'>
-            02 
-            <p className='ss'>Internal Structure</p>
-          </button>
-          <button className='info-type-btn'>
-            03
-            <p className='ss'>Surface Geology</p>
-          </button>
+          </div>
+          <div style={{display: 'none'}}>
+            {/* Here is overview, internal and surface buttons */}
+            <button className='info-type-btn'>
+              01 
+              <p className='ss'>Overview</p>
+            </button>
+            <button className='info-type-btn'>
+              02 
+              <p className='ss'>Internal Structure</p>
+            </button>
+            <button className='info-type-btn'>
+              03
+              <p className='ss'>Surface Geology</p>
+            </button>
+          </div>
         </div>
       </section>
       {/* Here is footer */}
